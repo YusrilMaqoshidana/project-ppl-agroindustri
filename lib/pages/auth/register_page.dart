492a564e9a1c0511c8a755cce15acd36d57fed20
yusril maqoshidana <yusrilkingsr07@gmail.com>
2024-04-03 21:54:28 +0700

    + FE: register done

--- lib/pages/auth/register_page.dart
+++ lib/pages/auth/register_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:gencoff_app/widgets/button.dart';
-import 'package:gencoff_app/widgets/gesture_detector.dart';
-import 'package:gencoff_app/widgets/input.dart';
+import 'package:gencoff_app/utils/button.dart';
+import 'package:gencoff_app/utils/gesture_detector.dart';
+import 'package:gencoff_app/utils/input.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
@@ -10,80 +10,106 @@ class RegisterPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
+        backgroundColor: Color.fromARGB(0, 0, 0, 0),
       ),
-      body: Container(
-        margin: EdgeInsets.all(25),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Register",
-              style: TextStyle(
-                fontFamily: "Inter",
-                fontWeight: FontWeight.w700,
-                fontSize: 28,
-                letterSpacing: 3,
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.only(left: 25, right: 25, top: 10, bottom: 5),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Register",
+                style: TextStyle(
+                  fontFamily: "Inter",
+                  fontWeight: FontWeight.w700,
+                  fontSize: 28,
+                  letterSpacing: 3,
+                ),
               ),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("Username"),
-                SizedBox(
-                  height: 5,
-                ),
-                InputWithoutIcon(text: "Masukan Username"),
-                SizedBox(
-                  height: 20,
-                ),
-                Text("Email"),
-                SizedBox(
-                  height: 5,
-                ),
-                InputWithoutIcon(text: "Email"),
-                SizedBox(
-                  height: 20,
-                ),
-                Text("Password"),
-                SizedBox(
-                  height: 5,
-                ),
-                InputPassword(text: "Masukan Password"),
-                SizedBox(
-                  height: 20,
-                ),
-                Text("Konfirmasi Password"),
-                SizedBox(
-                  height: 5,
-                ),
-                InputPassword(text: "Masukan Password"),
-              ],
-            ),
-            SizedBox(
-              height: 120,
-            ),
-            Button(text: "Daftar", onPressed: () {}),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Sudah punya akun?"),
-                Container(
-                    padding: EdgeInsets.only(left: 5),
-                    child: Gesture(
-                        text: "Login",
-                        onTap: () {
-                          Navigator.pushNamed(context, "/login");
-                        }))
-              ],
-            )
-          ],
+              SizedBox(
+                height: 40,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Username",
+                    style: TextStyle(
+                        fontFamily: "Inter",
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  InputWithoutIcon(text: "Masukan Username"),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Email",
+                    style: TextStyle(
+                        fontFamily: "Inter",
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  InputWithoutIcon(text: "Email"),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Password",
+                    style: TextStyle(
+                        fontFamily: "Inter",
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  InputPassword(text: "Masukan Password"),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Konfirmasi Password",
+                    style: TextStyle(
+                        fontFamily: "Inter",
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  InputPassword(text: "Masukan Password"),
+                ],
+              ),
+              SizedBox(
+                height: 100,
+              ),
+              Button(text: "Daftar", onPressed: () {}),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Sudah punya akun?"),
+                  Container(
+                      padding: EdgeInsets.only(left: 5),
+                      child: Gesture(
+                          text: "Login",
+                          onTap: () {
+                            Navigator.pushNamed(context, "/login");
+                          }))
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
