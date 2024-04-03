004662abcd624a903b6fd9e666692c9a5d714319
yusril maqoshidana <yusrilkingsr07@gmail.com>
2024-04-03 21:54:51 +0700

    + FE: login done

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -1,115 +1,114 @@
 import 'package:flutter/material.dart';
-import 'package:gencoff_app/widgets/button.dart';
-import 'package:gencoff_app/widgets/gesture_detector.dart';
-import 'package:gencoff_app/widgets/input.dart';
+import 'package:gencoff_app/pages/Homepage/first_page.dart';
+import 'package:gencoff_app/utils/button.dart';
+import 'package:gencoff_app/utils/gesture_detector.dart';
+import 'package:gencoff_app/utils/input.dart';
 
-class LoginPage extends StatefulWidget {
+class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "LOGIN",
-            style: TextStyle(
-              fontFamily: 'Inter',
-              fontWeight: FontWeight.w700,
-              fontSize: 28,
-              letterSpacing: 3,
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Image.asset('../assets/images/logo.png', width: 120),
-          SizedBox(
-            height: 40,
-          ),
-          Container(
-            margin: EdgeInsets.only(left: 25, right: 25),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Username",
-                  style: TextStyle(
-                      fontFamily: "Inter",
-                      fontSize: 18,
-                      fontWeight: FontWeight.w700),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                InputWithoutIcon(text: "Masukan Username"),
-                SizedBox(height: 10),
-                Text(
-                  "Password",
-                  style: TextStyle(
-                      fontFamily: "Inter",
-                      fontSize: 18,
-                      fontWeight: FontWeight.w700),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                InputPassword(text: "Masukan Password"),
-              ],
-            ),
-          ),
-          SizedBox(height: 10,),
-          Row(
-            mainAxisAlignment:
-                MainAxisAlignment.end, // Mengatur teks di ujung kanan
-            children: [
-              Container(
-                margin: EdgeInsets.only(right: 30),
-                child: Gesture(text: "Lupa password?", onTap: () {})),
-            ],
-          ),
-          SizedBox(height: 150),
-          Button(
-            text: "LOGIN",
-            onPressed: () {
-              Navigator.pushNamed(context, '/login');
-            },
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Row(
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.only(left: 25, right: 25, top: 100, bottom: 5),
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                "Belom punya akun owner?",
+                "LOGIN",
                 style: TextStyle(
-                    fontFamily: 'Inter',
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400),
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w700,
+                  fontSize: 28,
+                  letterSpacing: 3,
+                ),
               ),
               SizedBox(
-                width: 5,
+                height: 20,
               ),
-              Gesture(
-                text: "Register",
-                onTap: () {},
+              Image.asset('assets/images/logo.png', width: 100),
+              SizedBox(
+                height: 60,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Username",
+                    style: TextStyle(
+                        fontFamily: "Inter",
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  InputWithoutIcon(text: "Masukan Email"),
+                  SizedBox(height: 20),
+                  Text(
+                    "Password",
+                    style: TextStyle(
+                        fontFamily: "Inter",
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  InputPassword(text: "Masukan Password"),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment:
+                    MainAxisAlignment.end, // Mengatur teks di ujung kanan
+                children: [
+                  Gesture(
+                      text: "Lupa password?",
+                      onTap: () {
+                        Navigator.pushNamed(context, '/forgot_password');
+                      }),
+                ],
+              ),
+              SizedBox(height: 140),
+              Button(
+                text: "LOGIN",
+                onPressed: () {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => FirstPage()));
+                },
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Belom punya akun?",
+                    style: TextStyle(
+                        fontFamily: 'Inter',
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400),
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Gesture(
+                    text: "Register",
+                    onTap: () {
+                      Navigator.pushNamed(context, '/register');
+                    },
+                  )
+                ],
               )
             ],
-          )
-          // ElevatedButton(
-          //   child: Text("Go to Forgot Password"),
-          //   onPressed: (){
-          //   Navigator.pushNamed(context, '/forgot_password');
-          //   },
-          // ),
-        ],
+          ),
+        ),
       ),
     );
   }
