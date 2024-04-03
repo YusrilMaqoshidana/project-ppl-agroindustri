8ba52af4ca2d74e3dca5ac2c34a31fee3c388877
yusril maqoshidana <yusrilkingsr07@gmail.com>
2024-04-03 17:10:40 +0700

    + FE:forgot password

--- lib/pages/auth/forgot_password_page.dart
+++ lib/pages/auth/forgot_password_page.dart
@@ -1,24 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:gencoff_app/widgets/button.dart';
+import 'package:gencoff_app/widgets/input.dart';
 
-class ForgotPasswordPage extends StatefulWidget {
+class ForgotPasswordPage extends StatelessWidget {
   const ForgotPasswordPage({super.key});
 
   @override
-  State<ForgotPasswordPage> createState() => _ForgotPasswordPageState();
-}
-
-class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.white,),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+      ),
       body: Center(
-        child: ElevatedButton(
-          child: Text("Go to Recovery"),
-          onPressed: () {
-            Navigator.pushNamed(
-                context, '/recovery_password');
-          },
+        child: Container(
+          margin: EdgeInsets.all(25),
+          child: Column(
+            children: [
+              Text(
+                  "Lupa Password",
+                  style: TextStyle(
+                    fontFamily: "Inter",
+                    fontWeight: FontWeight.w700,
+                    fontSize: 28,
+                    letterSpacing: 3,
+                  ),),
+              Image.asset('../../../assets/images/forgot_password.png', width: 300,),
+              SizedBox(height: 200,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Email"),
+                  SizedBox(height: 5,),
+                  InputWithoutIcon(text: "Masukan Email"),
+                ],
+              ),
+              SizedBox(height: 40,),
+              Button(text: "Konfirmasi", onPressed: (){})
+            ],
+          ),
         ),
       ),
     );
