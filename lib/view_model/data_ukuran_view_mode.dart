482a0d21c2411af77d43752b43fda6e0345c5cb4
yusril maqoshidana <yusrilkingsr07@gmail.com>
2024-05-16 00:03:31 +0700

    fix: view model data ukuran done

--- lib/view_model/data_ukuran_view_mode.dart
+++ lib/view_model/data_ukuran_view_mode.dart
@@ -1,24 +1,46 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/widgets.dart';
+import 'package:gencoff_app/widgets/alert.dart';
 
-class DataSensorViewModel {
+class DataSortirViewModel {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  Future<void> addDataSensor( Map<String, int> hijau, Map<String, int> merah) async {
-    try {
-      String uid = _firebaseAuth.currentUser!.uid;
+  Future<void> addDataSensor(
+      Map<String, int> dataSensor,
+      Map<String, int> merah,
+      Map<String, int> hijau,
+      BuildContext context) async {
+    if (dataSensor.containsValue(0) &&
+        merah.containsValue(0) &&
+        hijau.containsValue(0)) {
+      FailAlertState(
+          message: "Data gagal ditambahkan",
+          onPressed: () {
+            Navigator.pop(context);
+          });
+    } else {
+      try {
+        String uid = _firebaseAuth.currentUser!.uid;
 
-      CollectionReference dataSensorCollection = _firestore.collection('users').doc(uid).collection('ukuran_biji');
+        CollectionReference dataSensorCollection = _firestore
+            .collection('users')
+            .doc(uid)
+            .collection('data_hasil_sortir');
 
-      await dataSensorCollection.add({
-        'hijau': hijau,
-        'merah': merah,
-        'waktu': FieldValue.serverTimestamp(), // Menambahkan informasi waktu saat ini
-      });
-      print('Data sensor berhasil ditambahkan.');
-    } catch (e) {
-      print('Terjadi kesalahan: $e');
+        await dataSensorCollection.add({
+          'data_sensor': dataSensor,
+          'data_ukuran_hijau': merah,
+          'data_ukuran_merah': hijau,
+          'waktu': FieldValue
+              .serverTimestamp(), // Menambahkan informasi waktu saat ini
+        });
+        SuccesAlertState(
+            message: "Data berhasil ditambahkan", onPressed: () {Navigator.pop(context);});
+      } catch (e) {
+        FailAlertState(message: "Data gagal ditambahkan", onPressed: () {Navigator.pop(context);});
+      }
     }
   }
 }
